6f76bc67a515b0fe3aa541b40c873e898fc5f0d2
Jordan <JDLChang@gmail.com>
2023-07-27 15:12:34 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import StickyNote from "./components/stickyNote"
+import "./App.css";
+
+function App() {
+  const [notes, setNotes] = useState([])
+  function addNote() {
+    setNotes([
+      ...notes,
+      {
+        id: Date.now(),
+      },
+    ])
+  }
+
+  function removeNote(noteId) {
+    setNotes(notes.filter((item => item.id !== noteId)))
+  }
+  return (
+    <div className="App" >
+      <button className="sticky-btn" onClick={addNote}>
+        Create New Note
+      </button>
+
+      {notes.map((item) => (
+        <StickyNote key={item.id} onClose={() => removeNote(item.id)} />
+      ))}
+    </div >
+  )
+}
+
+export default App
